c5fcd8df8d6fc26a3e117b80b0e9863f369b7655
Vitaly Chepyolkin <vitalik.chepyolkin@gmail.com>
2020-12-18 17:18:34 +0300

    ADDED: Main, Class Grphics Display

---
+++ src/bsu/rfe/java/group6/lab4/Chepyolkin/Main.java
@@ -0,0 +1,13 @@
+package bsu.rfe.java.group6.lab4.Chepyolkin;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
+
